4253b06352e004c22813a5c18a211361b1672862
sovonbd <sovonsdp@gmail.com>
2023-10-19 09:16:03 -0500

    removed index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config;
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,11 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// brandShop
-// CYmPUhFptV3K20Ci
-
-const uri =
-  "mongodb+srv://brandShop:CYmPUhFptV3K20Ci@cluster0.2dhdxvg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2dhdxvg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
