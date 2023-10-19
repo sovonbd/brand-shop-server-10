be3db96f3b07b744b1d6c55345f73c6e7fafb7b4
sovonbd <sovonsdp@gmail.com>
2023-10-19 13:14:28 -0500

    updated the api for id

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
 
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await foodieCollection.findOne(query);
       res.send(result);
